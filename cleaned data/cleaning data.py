cfe3fa74d3bf0c1870f3b1858c46252f0a350f7b
Neil Parsa <nkp305@gmail.com>
2023-08-19 19:54:02 -0400

    File Transfer

---
+++ cleaned data/cleaning data.py
@@ -0,0 +1,37 @@
+# importing libraries
+import pandas as pd
+
+# initializing the name of the file and the columns we want to include
+filename = 'NYCT Bus Tables - Average Weekday' + '.csv'
+cols = ['Route', '2016', '2017', '2018', '2019', '2020']
+
+# reading the csv file and putting it into a data frame
+file = pd.read_csv(filename, header = 1, usecols = cols)
+df = pd.DataFrame(file)
+#print(df)
+
+# removing the empty rows
+df = df.dropna()
+
+
+# finding the rows we want to remove
+words_to_remove = ['Local', 'Total', 'Exp', 'OTHER', 'X']
+rows_to_remove = [] # creating a list to put the names of the rows I want to remove
+
+for route in df['Route']:
+    for word in words_to_remove:
+        if word in route:
+            rows_to_remove.append(route)
+#print(rows_to_remove)
+
+
+# finding the index of the rows we want to remove
+for row in rows_to_remove:
+    #print(df.loc[df['Route'] == index].index)
+    filter_mask = df.loc[df['Route'] == row].index
+    df = df.drop(filter_mask)   # dropping the rows
+
+print(df)
+
+# putting the dataframe into a csv file
+df.to_csv('Average Weekday Ridership.csv', index = False)
---
+++ cleaned data/reformat_data.py
@@ -0,0 +1,72 @@
+# importing libraries
+import pandas as pd
+
+# creating a function to reformat the csv file
+def reformat(column_name):
+    # reading the csv file and putting it into a dataframe
+    filename = 'Average Weekday Ridership.csv'
+    file = pd.read_csv(filename, usecols=['Route', column_name]) # only using the selected columns
+    df = pd.DataFrame(file)
+    
+    
+    # inserting the year column
+    year_list = []
+    for i in range(len(df)):
+        year_list.append(column_name)
+    df.insert(0, 'Year', year_list)
+    
+    
+    # creating a dictionary 
+    borough = {'B': 'Brooklyn', 
+               'Bx': 'Bronx',
+               'M': 'Manhatten',
+               'Q': 'Queens',
+               'S': 'Staten Island'}
+    
+    # creating a list to add as a column
+    borough_list = []
+    
+    # matching the route with the borough
+    for route in df['Route']:
+        for key in borough:
+            if key == 'Bx' and 'Bx' in route:
+                borough_list.append(borough[key])
+            elif route[0] == key and route[0:2] != 'Bx':
+                #print(route)
+                borough_list.append(borough[key])
+    #print(borough_list)
+                
+    # inserting the borough column
+    df.insert(2, 'Borough', borough_list)
+    
+    # renaming the column as average ridership
+    df = df.rename(columns= {column_name: 'Avg. Ridership'})
+    
+    # returning the dataframe created
+    return df
+
+
+
+# creating a list to refomat each year
+years = ['2016', '2017', '2018', '2019', '2020']
+
+# creating a list to put each dataframe into
+dataframes = []
+
+# adding each dataframe into the list
+for year in years:
+    dataframes.append(reformat(year))
+    
+# combining all the dataframes into one
+df = pd.concat(dataframes, ignore_index = True)
+
+print(df) # printing the whole concatanated dataframe
+
+#print(df.describe())
+
+# putting the dataframe into a csv file
+df.to_csv('2nd Format.csv', index = False)
+
+    
+
+
